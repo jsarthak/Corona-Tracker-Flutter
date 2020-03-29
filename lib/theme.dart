28ddac509e5c35714ae750eeea5c81ae07a3c9e9
Sarthak Jain <jsarthak@outlook.com>
2020-03-29 16:35:09 +0530

    initial layouts

---
+++ lib/theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+final ThemeData appThemeMain = ThemeData(
+  brightness: Brightness.light,
+  primarySwatch: Colors.grey,
+  primaryColor: Colors.white,
+  primaryColorDark: Colors.white,
+  dialogTheme: DialogTheme(backgroundColor: Colors.white),
+  colorScheme: ColorScheme.light(),
+  canvasColor: Colors.transparent,
+  accentColor: Color.fromARGB(255, 155, 89, 182),
+);
